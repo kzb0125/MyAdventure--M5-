8df3559dca1f7efe7e6185b1bef1ae33e8103b33
baik <kzb0125@auburn.edu>
2024-02-08 15:14:02 -0800

    Initial Commit

---
+++ MyAdventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAdventure
+//
+//  Created by Baik on 2/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
